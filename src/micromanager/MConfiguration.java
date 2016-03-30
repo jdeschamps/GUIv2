109ddc077ed58d554424bed18e0972c8e4917747
jdeschamps <deschamps.joran@free.fr>
2016-03-30 12:49:39 +0200

    new BFP pos

--- src/micromanager/MConfiguration.java
+++ src/micromanager/MConfiguration.java
@@ -56,7 +56,7 @@ public class MConfiguration {
 	public static final String[] proplabel = {"Position","BFP State","3DA state"};
 	public static final int[] numposservo = {5,4,2};															//// here careful with the FW (num positions)
 	public static final String[] servokeys = {"FW","BFP","3DA"};
-	public static final int bfpPosition = 3;															/// here matters
+	public static final int bfpPosition = 1;															/// here matters, former 3
 	public static final int[] maestropos = {1950,1740,1500,1250,1040};
 
 	//////////////////////////////////////////////
